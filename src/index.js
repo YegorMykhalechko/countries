a8907acb233580d7c6dc6a4fcb30741ee15a7294
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-15 22:42:27 +0200

    env

---
+++ src/index.js
@@ -0,0 +1,4 @@
+'use strict'
+//https://restcountries.eu/
+
+
---
+++ src/js/errorNotice.js
@@ -0,0 +1,31 @@
+// import "./styles.css";
+// import { error } from "@pnotify/core";
+// import "@pnotify/core/dist/PNotify.css";
+// import "@pnotify/core/dist/BrightTheme.css";
+// import * as Confirm from "@pnotify/confirm";
+// import "@pnotify/confirm/dist/PNotifyConfirm.css";
+
+// function click() {
+//   error({
+//     title: "Button Clicked",
+//     text:
+//       "You have clicked the button. You may now complete the process of reading the notice.",
+//     modules: new Map([
+//       [
+//         Confirm,
+//         {
+//           confirm: true,
+//           buttons: [
+//             {
+//               text: "Ok",
+//               primary: true,
+//               click: notice => {
+//                 notice.close();
+//               }
+//             }
+//           ]
+//         }
+//       ]
+//     ])
+//   });
+// }
